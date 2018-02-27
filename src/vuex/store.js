a85ad5bac5abfd15a13e59f76243337ec2c1fb6a
yangyang <yangyang@phtfdata.com>
2018-02-27 11:23:11 +0800

    pc端提交

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,18 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import user from './modules/user'
-<<<<<<< HEAD
+
 import com from './modules/com'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-    modules: {
-        user,
-        com
-    }
-=======
-
 Vue.use(Vuex)
 const debug = process.env.NODE_EVN !== 'production' && process.env.NODE_EVN !== 'production-runtime'
 
@@ -22,7 +12,4 @@ export default new Vuex.Store({
     user,//用户信息
     com //公共信息
   },
-  strict: debug,
-  plugins: debug ? [createLogger()] : []
->>>>>>> 3da5c0ef830f134a1f18a569c120fdb31326c863
 })
