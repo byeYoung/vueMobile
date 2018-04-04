3421a8fb138fce4d445e5f2eed509180691c204d
taiyanghua <huruijia@phtfdata.com>
2018-04-04 16:31:37 +0800

    '22222'

--- src/pages/download/download.vue
+++ src/pages/download/download.vue
@@ -1,5 +1,84 @@
 <template>
+  <div id="download">
+    <!--头部-->
+    <div class="downBanner">
+      <div class="bannerBg">
+        <a  href="http://www.phtfdata.com/file/file/app/csdapp.apk" target="_blank" class="downloadAndroid"></a>
+        <a href="https://itunes.apple.com/cn/app/id961060424?mt=8" target="_blank" class="downloadIos"></a>
+      </div>
+    </div>
+    <!--免费充值  立享收益-->
+    <div class="downOne clearfix">
+      <div class="downOneLf"> </div>
+      <div class="downOneRt">
+        <h3>
+          <i></i>
+          免费充值  立享收益
+        </h3>
+        <p>免费充值  立享收益金梧桐全程免费为您服务，平台承诺不收取任何费用哦！不用出门，动动手指就赚钱啦！最高年化收益率达银行同期产品的25倍以上呢！</p>
+      </div>
+    </div>
+    <!--安全稳定  任性投资-->
+    <div class="w100 downTwo">
+      <div class="downTwoW100">
+        <div class="downLf downTwoLf">
+          <h3>
+            <i></i>
+            安全稳定  任性投资
+          </h3>
+          <p>安全稳定  任性投资专业人士为您精选优质项目，从根本上剔除潜在风险，让您在拥有高收益的同时可以更合理地配置资产。金梧桐采用“第三方资金托管”模式，优选合作机构，资金安全保障、极致透明，您再也不用担心投资安全问题啦！</p>
+        </div>
+        <div class="downRt downTwoRt"></div>
+      </div>
+    </div>
+    <!--稳盈高效  即买即赚-->
+    <div class="w100 downThree">
+      <div class="downThreeW100">
+        <div class="downRt downThreeRt"></div>
+        <div class="downLf downThreeLf">
+          <h3>
+            <i></i>
+            稳盈高效  即买即赚
+          </h3>
+          <p>金梧桐理财产品期限多样，定投盈天天盈任您挑选， 不管哪种投资，都将为您带来超越同类产品的收益，而且活动神马的超多哦！</p>
+        </div>
+      </div>
+    </div>
+    <div class="downFour">
+      <div class="downFourW100">
+        <div class="downLf downFourLf">
+          <h3>
+            <i></i>
+            稳盈高效  即买即赚
+          </h3>
+          <p>金梧桐理财产品期限多样，定投盈天天盈任您挑选， 不管哪种投资，都将为您带来超越同类产品的收益，而且活动神马的超多哦！</p>
+        </div>
+        <div class="downRt downFourRt"></div>
+      </div>
+    </div>
+    <div class="downFive">
+      <div class="downFiveW100">
+        <div class="downFiveBg clearfix">
+          <div class="downFiveLf">
+            <p>方法1:扫描二维码下载</p>
+            <img src="../../assets/images/download/download-img.jpg" alt="">
+          </div>
+          <div class="downFiveMl">
+            <p>方法2:直接电脑下载</p>
+            <div class="downIosAn">
+              <a href="http://www.phtfdata.com/file/file/app/csdapp.apk" target="_blank" class="downloadAndroid"></a>
+              <a href="https://itunes.apple.com/cn/app/id961060424?mt=8" target="_blank" class="downloadIos"></a>
+            </div>
+          </div>
+          <div class="downFiveRt">
+            <p>方法3:软件市场搜索“金梧桐”</p>
+            <p class="downFont">在APP store或应用市场直接搜索“<span>金梧桐</span>”即可下载。</p>
+          </div>
+        </div>
 
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,5 +86,264 @@
 </script>
 
 <style lang="less" scoped>
+  .downBanner{
+    width: 100%;
+    height:691px;
+    background: url("../../assets/images/download/downloadm-top.png") no-repeat center;
+    .bannerBg{
+      position: relative;
+      width: 1126px;
+      height:691px;
+      margin: 0 auto;
+      background: url("../../assets/images/download/downloadm-topc.png") no-repeat center;
+      a{
+        position: absolute;
+        right: 281px;
+        float: left;
+        width: 234px;
+        height: 62px;
+      }
+      .downloadAndroid{
+        top: 282px;
+        background: url("../../assets/images/download/downloadm-top-Android.png") no-repeat center;
+      }
+      .downloadAndroid:hover{
+        background: url("../../assets/images/download/downloadm-top-Androidhove.png") no-repeat center;
+      }
+      .downloadIos{
+        bottom: 266px;
+        background: url("../../assets/images/download/downloadm-top-ios.png") no-repeat center;
+      }
+      .downloadIos:hover{
+        background: url("../../assets/images/download/downloadm-top-ioshove.png") no-repeat center;
+      }
+    }
+  }
+  .downOne{
+    width:1000px;
+    margin:0 auto;
+    .downOneLf {
+      width: 476px;
+      height: 533px;
+      float: left;
+      margin-right: 26px;
+      background:url("../../assets/images/download/downloadmup-cen.png") no-repeat center;
+    }
+    .downOneRt{
+      width: 424px;
+      height: 166px;
+      float: left;
+      margin-top: 170px;
+      overflow: hidden;
+      p{
+        float: left;
+        font-size: 16px;
+        line-height: 30px;
+        color: #88899a;
+        margin-top: 14px;
+      }
+      h3{
+        width: 100%;
+        height: 52px;
+        line-height: 52px;
+        font-size: 40px;
+        color: #333;
+        overflow: hidden;
+        float: left;
+        i{
+          float: left;
+          width:52px;
+          height:45px;
+          margin: 4px 12px 0 0;
+          background: url("../../assets/images/download/downloadmup-cen-titleh2i.png") no-repeat center;
+        }
+      }
+    }
+  }
+  .w100{
+    width:100%;
+  }
+  .downTwo,.downThree{
+    width: 100%;
+    height: 532px;
+  }
+  .downFour{
+    height: 533px;
+  }
+  .downTwoW100,.downThreeW100,.downFourW100{
+    width: 1000px;
+    margin: 0 auto;
+  }
+   .downTwo,.downFour{
+     background: url("../../assets/images/download/grey.png") no-repeat center;
+   }
+  .downLf{
+    width: 424px;
+    float: left;
+    margin-top: 170px;
+    overflow: hidden;
+    p{
+      float: left;
+      font-size: 16px;
+      line-height: 30px;
+      color: #88899a;
+      margin-top: 14px;
+    }
+    h3{
+      width: 100%;
+      height: 52px;
+      line-height: 52px;
+      font-size: 40px;
+      color: #333;
+      overflow: hidden;
+      float: left;
+      i{
+        float: left;
+        width:52px;
+        height:45px;
+        margin: 4px 12px 0 0;
+
+      }
+    }
+  }
+  .downRt{
+    width: 475px;
+    height: 533px;
+    float: left;
+  }
+  .downTwoRt {
+    margin-left: 26px;
+    background:url("../../assets/images/download/downloadminvest-cen-b.png") no-repeat center;
+  }
+  .downThreeRt{
+    margin-right: 26px;
+    background:url("../../assets/images/download/downloadmmake-cen-img.png") no-repeat center;
+  }
+  .downFourRt{
+    margin-left: 450px;
+    background:url("../../assets/images/download/downloadm-finance-cen-img.png") no-repeat center;
+  }
+  .downTwoLf h3 i{
+    background: url("../../assets/images/download/downloadminvest-cen-titleh2i.png") no-repeat center;
+  }
+  .downThreeLf h3 i{
+    background: url("../../assets/images/download/downloadmmake-cen-titlei2.png") no-repeat center;
+  }
+  .downFourLf h3 i{
+    background: url("../../assets/images/download/downloadm-finance-cen-title2i.png") no-repeat center;
+  }
+  .downFour{
+    height: 533px;
+  }
+  .downFourW100{
+    position: relative;
+    width: 1000px;
+    height:533px;
+    margin: 0 auto;
+  }
+  .downFourLf{
+    width: 424px;
+    position: absolute;
+    left: 0;
+    top:0;
+  }
+  /*第五块内容*/
+  .downFive{
+    width: 100%;
+    background: #fff;
+    .downFiveW100{
+      width: 1000px;
+      margin: 0 auto;
+      p{
+        width: 100%;
+        height: 30px;
+        margin-bottom: 18px;
+        line-height: 30px;
+        overflow: hidden;
+        font-size: 18px;
+        color: #666;
+      }
+      .downFiveBg{
+        padding-top: 80px;
+        padding-bottom: 90px;
+        background: url("../../assets/images/download/installation.png") no-repeat center;
+        .downFiveLf{
+          float: left;
+          width: 33%;
+          text-align: center;
+        }
+        .downFiveMl{
+          float: left;
+          width: 33%;
+          text-align: center;
+        }
+        .downFiveRt{
+          float: left;
+          width: 33%;
+          text-align: center;
+        }
+      }
+    }
+  }
+  .downIosAn{
+    height:150px;
+    margin-left: 42px;
+
+  }
+  .downIosAn>a{
+    float: left;
+    width: 234px;
+    height: 62px;
+  }
+  .downIosAn .downloadAndroid{
+    background: url("../../assets/images/download/downloadm-top-Android.png") no-repeat center;
+  }
+  .downIosAn .downloadAndroid:hover{
+    background: url("../../assets/images/download/downloadm-top-Androidhove.png") no-repeat center;
+  }
+  .downIosAn .downloadIos{
+    bottom: 10px;
+    background: url("../../assets/images/download/downloadm-top-ios.png") no-repeat center;
+  }
+  .downIosAn .downloadIos:hover{
+    background: url("../../assets/images/download/downloadm-top-ioshove.png") no-repeat center;
+  }
 
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
